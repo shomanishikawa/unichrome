412c205891c5f67c39f8066cf336ec8a50059b6a
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2013-03-15 13:10:35 -0400

    adding all files from local v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unichrome::Application.config.secret_token = 'c26cab6d08e7fbf0831527828cdc87e5c408ae7674709b451ae90255e764eeaf534854c6d7b9cf22e02e1920c87a014d9b26e32fa45e999e0d3c9f2171c53c36'
