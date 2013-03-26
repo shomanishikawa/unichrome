e8d06deffdbcd9d58ba53a0519deffe4c6025033
Shoma Nishikawa <shoma.nishikawa@gmail.com>
2013-03-26 01:10:40 -0400

    flash the last box, and remember last input

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,20 +12,67 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require spectrum
 //= require_tree .
 
 $(function(){
-  
+
   $("#color-field").spectrum({
     color: "#ffffff",
     preferredFormat: "hex6",
     flat: true,
     move: function(color) {
-          var hex = color.toHexString();
-          $("body").css({background:hex});
-          $("input#color-field").attr('value', hex);
-    }
+      var hex = color.toHexString();
+      $("body").css({background:hex});
+      $("input#color-field").attr('value', hex);
+    },
+    showPalette: true,
+    showSelectionPalette: true,
+    palette: [ ],
+    localStorageKey: "spectrum.homepage"
   });
   
+  setToLast();
+  
+  flashLast($('#last.color-box'));
+  
+  $('form.new_color .actions input').click(function(e){
+    e.preventDefault();
+    $('.sp-button-container button.sp-choose').click();
+    $('form.new_color .actions input').unbind().click();
+  });
+
+  
+
 });
+
+function setToLast(){
+  var lastColor = rgb2hex($('.sp-palette-row-selection span:first-child span').css('background-color'));
+  $("#color-field").spectrum("set", lastColor);
+}
+
+function flashLast(lastBox){
+  lastBox.css("border-color", isDark($('body').css("background-color")) ? 'white' : 'black');
+  lastBox.animate({opacity:'0'}, 200, 'easeOutQuad', function(){
+    $(this).animate({opacity:'1'}, 200, 'easeOutQuad', function(){
+      flashLast(lastBox);
+    });
+  });
+}
+
+function isDark( color ) {
+  var match = /rgb\((\d+).*?(\d+).*?(\d+)\)/.exec(color);
+  return parseFloat(match[1])
+  + parseFloat(match[2])
+  + parseFloat(match[3])
+  < 3 * 256 / 2; // r+g+b should be less than half of max (3 * 256)
+}
+
+function rgb2hex(rgb){
+ rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+ return "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2);
+}
